304411b3a2b9d308b448a71ded59b67c87d623ab
SohJorgeMesmo78 <jf78pereira@gmail.com>
2025-04-10 15:39:01 -0300

    Adicionando setas para proximo pokemon

--- src/app/pages/pokemon/pokemon.component.ts
+++ src/app/pages/pokemon/pokemon.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PokemonService } from '../../services/pokemon.service';
 import { Pokemon } from '../../interfaces/pokemon.model';
 
@@ -16,14 +16,38 @@ export class PokemonComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private pokemonService: PokemonService
+    private pokemonService: PokemonService,
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
-    this.pokemonId = Number(this.route.snapshot.paramMap.get('id'));
-    this.pokemonService.getPokemonById(this.pokemonId).subscribe({
-      next: (data) => this.pokemon = data,
-      error: (err) => console.error('Erro ao carregar Pokémon:', err),
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pokemonId = id;
+    this.buscarPokemon(id);
+  }
+  
+
+  carregarPokemon(id: number): void {
+    if (isNaN(id) || id < 1) return;
+    this.loading = true;
+    this.router.navigate(['/pokemon', id]);
+  
+    this.buscarPokemon(id)
+  }
+  
+
+  buscarPokemon(id: number): void {
+    this.loading = true;
+    this.pokemonService.getPokemonById(id).subscribe({
+      next: (data) => {
+        this.pokemon = data;
+        this.pokemonId = id;
+      },
+      error: (err) => {
+        console.error('Erro ao carregar Pokémon:', err);
+        alert('Ocorreu um erro ao buscar o Pokémon.');
+        this.loading = false;
+      },      
       complete: () => this.loading = false
     });
   }
